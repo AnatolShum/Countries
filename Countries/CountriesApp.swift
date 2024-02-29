e1b294cb38316edbb25584d6eb513c97d7b47cd8
Anatolii Shumov <seminarsds@gmail.com>
2024-02-29 09:28:33 +0100

    initial commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Anatolii Shumov on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
